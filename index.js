5b6135c80374136a0d8483d02bd31fac7fab8269
786565784@qq.com <liangyuan.deng@epro.com.cn>
2024-04-11 15:07:16 +0800

    重置默认设置

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import RTNSensors from "./NativeSensors";
-import { DeviceEventEmitter } from 'react-native'
-import { Observable } from 'rxjs'
-import { publish, refCount } from "rxjs/operators"
-import RTNSensors from './NativeSensors'
+import { DeviceEventEmitter } from "react-native";
+import { Observable } from "rxjs";
+import { publish, refCount } from "rxjs/operators";
+import RTNSensors from "./NativeSensors";
 
 const listenerKeys = new Map([
   ["accelerometer", "accelerometerClick"],
@@ -20,7 +20,7 @@ const SensorType = {
   barometer: "accelerometer",
   orientation: "accelerometer",
   gravity: "accelerometer",
-}
+};
 
 const eventEmitterSubscription = new Map([
   ["accelerometer", null],
@@ -33,69 +33,68 @@ const eventEmitterSubscription = new Map([
 
 function createSensorObservable(sensorType) {
   return Observable.create(function subscribe(observer) {
-    this.isSensorAvailable = false
+    this.isSensorAvailable = false;
     if (eventEmitterSubscription.get(sensorType)) {
-      eventEmitterSubscription.get(sensorType).remove()
+      eventEmitterSubscription.get(sensorType).remove();
       RTNSensors.off(sensorType);
-
     }
     this.unsubscribeCallback = () => {
       if (!this.isSensorAvailable) return;
       if (eventEmitterSubscription.get(sensorType)) eventEmitterSubscription.get(sensorType).remove();
       RTNSensors.off(sensorType);
-    }
-    RTNSensors.isAvailable(SensorType).then(
-      (res) => {
-        this.isSensorAvailable = true;
-        if (!res) {
-          observer.error(`Sensors${sensorType} is not avilable`);
-          return
-        } else {
-          switch (sensorType) {
-            case 'accelerometer':
-              RTNSensors.accelerometer();
-              break;
-            case 'gyroscope':
-              RTNSensors.gyroscope();
-              break;
-            case 'magnetometer':
-              RTNSensors.magnetometer();
-              break;
-            case 'barometer':
-              RTNSensors.barometer();
-              break;
-            case 'orientation':
-              RTNSensors.orientation();
-              break;
-            case 'gravity':
-              RTNSensors.gravity();
-              break;
-            default:
-          }
-          eventEmitterSubscription.set(
-            sensorType,
-            DeviceEventEmitter.addListener(listenerKeys.get(sensorType), (date) => {
-              observer.next(date);
-            })
-          )
+      RTNSensors.setUpdateInterval(sensorType, 0);
+      RTNSensors.setLogLevel(sensorType, 0);
+    };
+    RTNSensors.isAvailable(SensorType).then((res) => {
+      this.isSensorAvailable = true;
+      if (!res) {
+        observer.error(`Sensors${sensorType} is not avilable`);
+        return;
+      } else {
+        switch (sensorType) {
+          case "accelerometer":
+            RTNSensors.accelerometer();
+            break;
+          case "gyroscope":
+            RTNSensors.gyroscope();
+            break;
+          case "magnetometer":
+            RTNSensors.magnetometer();
+            break;
+          case "barometer":
+            RTNSensors.barometer();
+            break;
+          case "orientation":
+            RTNSensors.orientation();
+            break;
+          case "gravity":
+            RTNSensors.gravity();
+            break;
+          default:
         }
+        eventEmitterSubscription.set(
+          sensorType,
+          DeviceEventEmitter.addListener(listenerKeys.get(sensorType), (date) => {
+            observer.next(date);
+          })
+        );
       }
-    )
+    });
     return this.unsubscribeCallback;
   }).pipe(makeSingleton);
 }
 function makeSingleton() {
   return (source) => source.pipe(publish(), refCount());
 }
-export const accelerometer = createSensorObservable('accelerometer');
-export const gyroscope = createSensorObservable('gyroscope');
-export const magnetometer = createSensorObservable('magnetometer');
-export const barometer = createSensorObservable('barometer');
-export const orientation = createSensorObservable('orientation');
-export const gravity = createSensorObservable('gravity');
+export const accelerometer = createSensorObservable("accelerometer");
+export const gyroscope = createSensorObservable("gyroscope");
+export const magnetometer = createSensorObservable("magnetometer");
+export const barometer = createSensorObservable("barometer");
+export const orientation = createSensorObservable("orientation");
+export const gravity = createSensorObservable("gravity");
 export function setUpdateIntervalForType(type, updateInterval) {
   RTNSensors.setUpdateInterval(type, updateInterval);
 }
-export function setLogLevelForType(sensorType,logLevel){
-  RTNSensors.setLogLevel(sensorType,logLevel)
+export function setLogLevelForType(sensorType, logLevel) {
+  RTNSensors.setLogLevel(sensorType, logLevel);
 }
